68e3bfc2afdc57499dbd20a151582cbd50509c70
Arthur Williams <arthurwiliams86@gmail.com>
2023-12-07 02:07:45 +0300

    chore: Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+import DEFAULT_THEME from 'tailwindcss/defaultTheme';
+
+module.exports = {
+  content: ["./src/**/*.{html,css,ts}"],
+  theme: {
+    colors: {
+      "light-cyan": "hsl(193, 38%, 86%)",
+      "neon-green": "hsl(150, 100%, 66%)",
+      "grayish-blue": "hsl(217, 19%, 38%)",
+      "dark-grayish-blue": "hsl(217, 19%, 24%)",
+      "dark-blue": "hsl(218, 23%, 16%)"
+    },
+    fontFamily: {
+      sans: ["Manrope", ...DEFAULT_THEME.fontFamily.sans]
+    },
+  },
+}
+
